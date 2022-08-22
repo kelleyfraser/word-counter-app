28a5408525caa17d2d3e2ca6fb945618b774925d
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-08-22 16:55:19 -0400

    Add functionality to reset button.

--- src/components/appHeader.jsx
+++ src/components/appHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AppHeader = ({resetWordCounter}) => {
 return (
   <div className="header-container container-fluid">
-    <i className="fa fa-refresh btn-icon"></i>
+    <i onClick={resetWordCounter} className="fa fa-refresh btn-icon"></i>
     <h1 className="appName">word counter</h1>
   </div>
   );

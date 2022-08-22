880b4fce6b2e495145386a91212ead73e8b4ed77
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-08-22 11:37:42 -0400

    Build inputForm component.

---
+++ src/components/inputForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const InputForm = ({saveInputData}) => {
+
+  return (
+    <div className="form-container container-fluid text-center">
+      <form>
+        <div className="input-group mt-3">
+          <textarea 
+            className="form-control"
+            rows="5"
+            required />
+        </div>
+        <button className="btn btn-primary mt-3">
+            Count
+        </button> 
+      </form>
+    </div>
+  );
+}
+
+export default InputForm;

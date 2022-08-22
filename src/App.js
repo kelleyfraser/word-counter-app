f2b0fcddd6b28e6ed37658a7fb5477a2a7a88a66
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-08-22 17:28:23 -0400

    Completed App.js using custom components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import AppHeader from './components/appHeader';
+import InputForm from './components/inputForm';
+import ResultList from './components/resultList';
+import useCountState from './hooks/useCountState';
 import './App.scss';
 
 function App() {
+  const saveInputData = ((text) => {
+    const trimmedText = text.trim();
+    if (trimmedText.length > 0) {
+      countText(trimmedText);
+    }
+  });
+
+  const {counts, countText, inputText, resetWordCounter} = useCountState([]);
+
   return (
     <div className="App">
-      
+      <AppHeader resetWordCounter={resetWordCounter} />
+      <InputForm saveInputData={saveInputData}/>
+      <ResultList text={inputText} counts={counts} countText={countText} />
     </div>
   );
 }
